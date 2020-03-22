b91c28899a043b1a430613f10bd74adb20d7fcb9
dmpalewski <dmpalewski@gmail.com>
2020-03-22 15:59:29 +0100

    completed stage #1

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -1,7 +1,14 @@
 package readability;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder text = new StringBuilder();
+        while (scanner.hasNext()) {
+            text.append(scanner.nextLine());
+        }
+        System.out.print(text.length() > 100 ? "HARD" : "EASY");
     }
 }
